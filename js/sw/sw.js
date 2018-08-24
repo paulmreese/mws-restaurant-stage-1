11d25fdb8dc3a9bd06b78116113c5adf2edcbdd6
paulmreese <paul.m.reese@gmail.com>
2018-08-24 14:59:57 -0400

    Implement caching

--- js/sw/sw.js
+++ js/sw/sw.js
@@ -1,25 +1,26 @@
-let staticCacheName = 'restaurant-reviews-static-v1';
+let staticCacheName = 'restaurant-reviews-static-v5';
 
+//create a list of the images, adds images (as they are numbered 1-10)
+let listOfResources = [];
+for (let i = 1; i <= 10; i++) {
+  listOfResources.push('/img/' + i + '.jpg');
+}
+//add main pages to resources for caching
+listOfResources.push('/index.html', '/js/main.js', '/js/dbhelper.js',
+  '/css/styles.css')
+
+//create cache and store resources when service worker first installs
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
-
-      //create a list of the images, as they are numbered 1-10
-      let listOfImages = [];
-      for (let i = 1; i <= 10; i++) {
-        listOfImages.push('img/' + i + '.jpg');
-      }
-
-      return cache.addAll([
-        'js/main.js',
-        'js/dbhelper.js',
-        'css/styles.css',
-        listOfImages
-      ]);
+      return cache.addAll(
+        listOfResources
+      );
     })
   );
 });
 
+//when this worker activates, remove old versions of cache
 self.addEventListener('activate', function(event) {
   event.waitUntil(
     caches.keys().then(function(cacheNames) {
@@ -35,6 +36,7 @@ self.addEventListener('activate', function(event) {
   );
 });
 
+//responds with the cached resources first
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
